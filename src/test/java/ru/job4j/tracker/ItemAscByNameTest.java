e3d7983fefc6ff8182f6f0e2402df6bc6146d4a9
KatUser <ekaterina.shirokikh@gmail.com>
2024-11-26 17:41:45 +0700

    1. Конфигурация Hibernate [#6872]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -1,26 +1,30 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ItemAscByNameTest {
+
     @Test
     public void whenSortAscendingOrder() {
+        var nowDate = LocalDateTime.now();
         List<Item> item = new ArrayList<>(List.of(
-                new Item("Third", 3),
-                new Item("Fourth", 4),
-                new Item("First", 1),
-                new Item("Second", 2)
+                new Item("Third", 3, nowDate),
+                new Item("Fourth", 4, nowDate),
+                new Item("First", 1, nowDate),
+                new Item("Second", 2, nowDate)
         ));
 
         List<Item> expected = new ArrayList<>(List.of(
-                new Item("First", 1),
-                new Item("Fourth", 4),
-                new Item("Second", 2),
-                new Item("Third", 3)
+                new Item("First", 1, nowDate),
+                new Item("Fourth", 4, nowDate),
+                new Item("Second", 2, nowDate),
+                new Item("Third", 3, nowDate)
 
         ));
         item.sort(new ItemAscByName());
--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,20 +12,22 @@ public class ItemDescByNameTest {
 
     @Test
     public void whenSortDescendingOrder() {
+        var nowDate = LocalDateTime.now();
         List<Item> item = new ArrayList<>(List.of(
-                new Item("Third", 3),
-                new Item("Fourth", 4),
-                new Item("First", 1),
-                new Item("Second", 2)
+                new Item("Third", 3, nowDate),
+                new Item("Fourth", 4, nowDate),
+                new Item("First", 1, nowDate),
+                new Item("Second", 2, nowDate)
         ));
 
         List<Item> expected = new ArrayList<>(List.of(
-                new Item("Third", 3),
-                new Item("Second", 2),
-                new Item("Fourth", 4),
-                new Item("First", 1)
+                new Item("Third", 3, nowDate),
+                new Item("Second", 2, nowDate),
+                new Item("Fourth", 4, nowDate),
+                new Item("First", 1, nowDate)
         ));
         item.sort(new ItemDescByName());
+
         assertEquals(item, expected);
     }
 }
