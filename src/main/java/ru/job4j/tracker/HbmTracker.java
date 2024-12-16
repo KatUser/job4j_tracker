917ddd141bc8bb3747c893d9eb8132ca0fd7c5be
KatUser <ekaterina.shirokikh@gmail.com>
2024-12-16 15:56:50 +0700

    3. HbmTracker   [#287705]

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,131 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sessionFactory = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        boolean isReplaced = false;
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("""
+                            UPDATE Item
+                            SET name = :fName, created = :fCreated
+                            WHERE id = :fid
+                            """, Item.class)
+                    .setParameter("fName", item.getName())
+                    .setParameter("fCreated", item.getCreated())
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+            isReplaced = true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return isReplaced;
+    }
+
+    @Override
+    public void delete(int id) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE Item WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> foundItems = Collections.emptyList();
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("from Item", Item.class);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return foundItems;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sessionFactory.openSession();
+        List<Item> foundItems = Collections.emptyList();
+        try {
+            session.beginTransaction();
+            foundItems = session.createQuery(
+                            "from Item as i WHERE i.name = :fKey", Item.class
+                    )
+                    .setParameter("fKey", key)
+                    .getResultList();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return foundItems;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Session session = sessionFactory.openSession();
+        Item foundItem = null;
+        try {
+            session.beginTransaction();
+            foundItem = session.createQuery(
+                            "from Item as i WHERE i.id = :fId", Item.class
+                    )
+                    .setParameter("fId", id)
+                    .uniqueResult();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return foundItem;
+    }
+
+    @Override
+    public void close() {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
--- src/main/resources/db/scripts/001_ddl_create_items_table.sql
+++ src/main/resources/db/scripts/001_ddl_create_items_table.sql
@@ -1,4 +1,4 @@
-create table items (
+create table items if not exists (
     id serial primary key,
     name text,
     created timestamp
